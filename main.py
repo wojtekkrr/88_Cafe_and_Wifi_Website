e09bbf7bf2261aee05016f687badf2b048d92de9
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-09-20 17:17:32 +0200

    Dodany formularz z logowaniem i działa.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("elements.html")
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        contact_data = request.form
+        print(contact_data["demo-name"])
+        # contact_data["email"], contact_data["message"])
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
