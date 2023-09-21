3f70196aa5e5d1aa0e36d79431b811a8466824c5
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-09-21 08:23:37 +0200

    Przygotowane strony login i register.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
 
 
 @app.route("/login", methods=["POST"])
@@ -15,6 +15,10 @@ def login():
         print(contact_data["demo-name"])
         # contact_data["email"], contact_data["message"])
         return redirect(url_for('home'))
+
+@app.route("/register", methods=["POST"])
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
