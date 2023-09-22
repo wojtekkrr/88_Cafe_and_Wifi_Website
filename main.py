46d5970cd6fd9e373ec02ab955f319ee622b9b5a
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-09-22 08:02:00 +0200

    Działa dodawanie kawiarni.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, url_for, redirect, request, abort
+from flask import Flask, render_template, url_for, redirect, request, abort, flash
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 # Configure Flask-Login
 login_manager = LoginManager()
@@ -42,7 +43,7 @@ class Cafe(db.Model):
 
 
 # User table for all your registered users
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
@@ -71,20 +72,62 @@ def admin_only(f):
 def home():
     result = db.session.execute(db.select(Cafe))
     cafes = result.scalars().all()
-    return render_template("index.html", all_cafes=cafes)
+    return render_template("index.html", all_cafes=cafes, current_user=current_user)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        contact_data = request.form
-        # contact_data["email"], contact_data["message"])
-    return render_template("login.html")
+        login_data = request.form
+        result = db.session.execute(db.select(User).where(User.email == login_data["email"]))
+        # Note, email in db is unique so will only have one result.
+        user = result.scalar()
+        # Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        # Password incorrect
+        elif not check_password_hash(user.password, login_data["password"]):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('home'))
+    return render_template("login.html", current_user=current_user)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        data = request.form
+        # Check if user email is already present in the database.
+        result = db.session.execute(db.select(User).where(User.email == data["email"]))
+        user = result.scalar()
+        if user:
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
+        hash_and_salted_password = generate_password_hash(
+            data["password"],
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(
+            email=data["email"],
+            name=data["name"],
+            password=hash_and_salted_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("register.html", current_user=current_user)
 
 # Add a POST method to be able to post comments
 @app.route("/cafe/<int:cafe_id>", methods=["GET", "POST"])
@@ -105,8 +148,62 @@ def show_cafe(cafe_id):
     #     )
     #     db.session.add(new_comment)
     #     db.session.commit()
-    return render_template("cafe.html", cafe=requested_cafe)#, current_user=current_user, form=comment_form)
+    return render_template("cafe.html", cafe=requested_cafe, current_user=current_user)
 
+
+# Use a decorator so only an admin user can delete a post
+@app.route("/delete/<int:cafe_id>")
+@admin_only
+def delete_post(cafe_id):
+    post_to_delete = db.get_or_404(Cafe, cafe_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+# Use a decorator so only an admin user can create new posts
+@app.route("/new-post", methods=["GET", "POST"])
+@admin_only
+def add_new_post():
+    if request.method == "POST":
+        post_data = request.form
+
+        if post_data["has_toilet"] == "on":
+            toilets = True
+        else:
+            toilets = False
+
+        if post_data["has_wifi"] == "on":
+            wifi = True
+        else:
+            wifi = False
+
+        if post_data["has_sockets"] == "on":
+            sockets = True
+        else:
+            sockets = False
+
+        if post_data["can_take_calls"] == "on":
+            calls = True
+        else:
+            calls = False
+
+        new_post = Cafe(
+            name=post_data["name"],
+            map_url=post_data["map_url"],
+            img_url=post_data["img_url"],
+            location=post_data["location"],
+            seats=post_data["seats"],
+            has_toilet=toilets,
+            has_wifi=wifi,
+            has_sockets=sockets,
+            can_take_calls=calls,
+            coffee_price=post_data["coffee_price"],
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("make-post.html", current_user=current_user)
 
 
 if __name__ == "__main__":
